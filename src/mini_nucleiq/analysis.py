8d01e5e6a1cac42b6b770aa7f2905d58fb005b25
cavundercover <carlosvilasalvarez+undercover@gmail.com>
2026-07-20 08:18:05 +0200

    Implement analyze_sample (green)

--- src/mini_nucleiq/analysis.py
+++ src/mini_nucleiq/analysis.py
@@ -2,6 +2,11 @@ from dataclasses import dataclass
 from enum import StrEnum
 
 from mini_nucleiq import samples
+from mini_nucleiq.algorithms import (
+    ALGORITHMS,
+    AlgorithmDecision,
+    run_algorithm,
+)
 
 
 class AnalysisDecision(StrEnum):
@@ -17,4 +22,17 @@ class AnalysisResult:
 def analyze_sample(
     sample_name: str, algorithms: list[str], samples_client: samples.SamplesClient
 ) -> AnalysisResult:
-    raise NotImplementedError
+    sample = samples_client.get_sample(sample_name=sample_name)
+    algorithm_results = []
+    for name in algorithms:
+        algorithm_results.append(run_algorithm(sample, ALGORITHMS[name]))
+    positives = 0
+    for result in algorithm_results:
+        if result.decision is AlgorithmDecision.POSITIVE:
+            positives += 1
+    decision = (
+        AnalysisDecision.POSITIVE
+        if positives * 2 > len(algorithms)
+        else AnalysisDecision.NEGATIVE
+    )
+    return AnalysisResult(decision=decision)
